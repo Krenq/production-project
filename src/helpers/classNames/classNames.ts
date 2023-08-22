e6e6a99bbda354d3dccf98ef7ce68ee5b294f529
Mike <petrovmike1998@gmail.com>
2023-08-22 23:24:18 +0300

    first commit

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,15 @@
+type Mods = Record<string, boolean | string>;
+
+export function classNames(
+  mainCls: string,
+  mods: Mods,
+  arrCls: string[]
+): string {
+  return [
+    mainCls,
+    ...arrCls,
+    ...Object.entries(mods)
+      .filter(([className, bool]) => Boolean(bool))
+      .map((cortageEl) => cortageEl[0]),
+  ].join(" ");
+}
